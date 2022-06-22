9295ee0d44c5b730bd8031df3a6f69e4e4d75652
Lingyv77 <Lingyv77@163.com>
2022-06-23 02:39:10 +0800

    1.0.0

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+
+module.exports = {
+  publicPath: "./",
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views'
+      }
+    },
+    devServer: {
+      open: false,
+      port: 8080, // 自定义修改8080端口
+    },
+    plugins: [
+      new webpack.BannerPlugin('所有版权归Lingyv77所有')
+    ],
+    mode: 'production'
+  },
+}
